fe95530fcd70da1299fb4a00738fa9bc9adfdff4
Diwakar-naik <137063932+Diwakar-naik@users.noreply.github.com>
2023-09-24 20:30:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	 const firstName = document.getElementById('form1').value;
+    const lastName = document.getElementById('form1').value;
 	alert("First Name: " + firstName + "\nLast Name: " + lastName);
 
 }
